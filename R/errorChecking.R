764d0a1899826361ddeaabacbcba614ca6883a0a
malte <malte@lana-labs.com>
2019-01-17 11:34:32 +0100

    Changed the error checking

--- R/errorChecking.R
+++ R/errorChecking.R
@@ -1,14 +1,17 @@
 checkHttpErrors <- function(response) {
 
   if(response["status_code"] != 200) {
-    if(!isEmptyLog(response)) {
       stop(paste0(httr::content(response), " \nHTTP error details = ", httr::http_status(response)$message))
-    } else {
+  }
+
+  if(!isEmptyLog(response)) {
       warning("No response data. Your filter settings led to an empty log.\n")
-    }
   }
 }
 
 isEmptyLog <- function(response) {
-  return(any(httr::content(response) == "EmptyLog"))
+  responseData <- jsonlite::fromJSON(httr::content(response, as = "text", encoding = "UTF-8"))
+  if(length(responseData$chartValues)==0) {
+    return(TRUE)
+  }
 }
