2fa3aa2439346a43cba8ab06f00f82e38d055079
malte <malte@lana-labs.com>
2019-03-12 15:50:25 +0100

    redone the checking for the discovered call

--- R/discoveredModel.R
+++ R/discoveredModel.R
@@ -33,14 +33,18 @@ discoveredModel <- function(lanaUrl, lanaToken, logId, traceFilterSequence="[]",
 
   # Check the variables being tramsitted from LANA and receiving the user ID.
 
-  rqBody <- lanar::buildVariantFilterSettings(selectedLogId, traceFilterSequence, "true")
+  rqBody <- lanar::buildVariantFilterSettings(logId, traceFilterSequence, "true")
 
 
   discoveredModelRequestData <- httr::GET(paste0(lanaUrl, "/api/discoveredModelWithFilter?request=", URLencode(rqBody, reserved = T)),
                                           httr::add_headers(Authorization = lanaToken))
 
 
+  if (!discoveredModelRequestData$status_code==400){
     discoveredModelData <- jsonlite::fromJSON(httr::content(discoveredModelRequestData, as = "text", encoding = "UTF-8"))
+    } else {
+      return(NULL)
+  }
 
   return(discoveredModelData)
 }
