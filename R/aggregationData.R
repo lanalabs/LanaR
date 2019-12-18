c414141723e82fb32a50a0b0010acbf82a9a92c8
KevinYang <kevin@lanalabs.com>
2019-12-18 10:35:22 +0100

    add required parameters

--- R/aggregationData.R
+++ R/aggregationData.R
@@ -1,6 +1,6 @@
 # build aggregation settings for the API call
 
-buildAggregationSettings <- function(xDimension, yDimension, logId, zDimension, aggrLevel, followers, type, cache, maxValueAmount, activityExclusionFilter, traceFilterSequence, limit, page) {
+buildAggregationSettings <- function(valuesFrom, extractedValue, aggregationType, xDimension, yDimension, logId, zDimension, aggrLevel, followers, type, cache, maxValueAmount, activityExclusionFilter, traceFilterSequence, limit, page) {
 
   if (zDimension != "null" ){
     zDimension = paste0('"', zDimension, '"')
@@ -10,7 +10,26 @@ buildAggregationSettings <- function(xDimension, yDimension, logId, zDimension, 
     followers = paste0('"', zDimension, '"')
   }
 
-  rqBody <- paste0('
+  if (valuesFrom %in% c("allCases", "allEvents", "allFollowers")){
+    valuesFrom = paste0('{"type": "', valuesFrom, '"}')
+  } else if (grepl(",", valuesFrom)){
+    events = unlist(strsplit(valuesFrom, "[,]"))
+    valuesFrom = paste0('{"type": "follower", 
+      "preActivity": "', events[1], '",
+      "succActivity": "', events[2], '"}')
+  } else {
+    valuesFrom = paste0('{"type": "event",
+      "activity": "', valuesFrom, '"}')
+  }
+
+  if (extractedValue %in% c("frequency", "duration", "startDate", "endDate")){
+    extractedValue = paste0('{"type": "', extractedValue, '"}')
+  } else {
+    extractedValue = paste0('{"type": "attributeValue", 
+      "attribute": "', extractedValue, '"}') 
+  }
+
+  miningBody <- paste0('
          {
          "xDimension": "', xDimension, '",
          "yDimension": "', yDimension, '",
@@ -32,12 +51,22 @@ buildAggregationSettings <- function(xDimension, yDimension, logId, zDimension, 
            "page": ', page, '
           }
          }')
+
+  rqBody <- paste0('{
+    "valuesFrom": ', valuesFrom, ',
+    "extractedValue": ', extractedValue, ',
+    "aggregationType": ', aggregationType, ',
+    "miningRequest": ', miningBody,'
+    }')
 }
 
 #' @title Aggregate
 #' Aggregate data once uploaded to Lana
 #' Aggregations can be calculated by time (month, day of week, hour) or by attribute regarding the frequency, average duration, median duration and total duration. Also the aggregated data can be grouped by attributes.
 #' @description Gets the aggregation of the requested data with the specified parameters . \cr See https://api.lana-labs.com/#/routes/getAggregatedData
+#' @param valuesFrom ("allCases" / "allEvents" / "allFollowers" / "event" / "followers")
+#' @param extractedValue ("frequency" / "duration" / "startDate" / "endDate" / "custom")
+#' @param aggregationType ("min" / "max" / "sum" / "mean" / "median")
 #' @param lanaUrl URL of the instance that LANA is running on
 #' @param lanaToken Lana API token read from LANA
 #' @param logId Log ID being read from LANA
@@ -61,16 +90,16 @@ buildAggregationSettings <- function(xDimension, yDimension, logId, zDimension, 
 #' aggregate("Incident_withImpactAttributes.csv", xDimension = "byTime=byMonth", yDimension = "totalDuration")
 #' aggregate("Incident_withImpactAttributes.csv", xDimension = "byTime=byMonth", yDimension = "frequency", zDimension = "byAttribute=Est. Cost")
 
-aggregate <- function(lanaUrl, lanaToken, logId, xDimension, yDimension, zDimension="null", aggrLevel="traces", followers="null",
+aggregate <- function(lanaUrl, lanaToken, logId, xDimension, yDimension, valuesFrom, extractedValue, aggregationType="null", zDimension="null", aggrLevel="traces", followers="null",
                       type="aggregation", cache="{}", maxValueAmount=5, activityExclusionFilter="[]", traceFilterSequence="[]",
                       limit = 10, page = 1){
 
   # Make request to get aggregated data from LANA
 
-  rqBody <- buildAggregationSettings(xDimension, yDimension, logId, zDimension, aggrLevel, followers, type, cache,
+  rqBody <- buildAggregationSettings(valuesFrom, extractedValue, aggregationType, xDimension, yDimension, logId, zDimension, aggrLevel, followers, type, cache,
                                      maxValueAmount, activityExclusionFilter, traceFilterSequence, limit, page)
 
-  aggregationRequestData <- httr::GET(paste0(lanaUrl, "/api/aggregatedData?request=", URLencode(rqBody, reserved = T)),
+  aggregationRequestData <- httr::GET(paste0(lanaUrl, "/api/v2/aggregatedData?request=", URLencode(rqBody, reserved = T)),
                                       httr::add_headers(Authorization = lanaToken)
                                       )
 
