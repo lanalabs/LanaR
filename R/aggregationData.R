5fca73e9871b4c3550901dc70e2c56e4745eb980
KevinYang <kevin@lanalabs.com>
2019-12-18 11:31:19 +0100

    add comments and fix some bugs

--- R/aggregationData.R
+++ R/aggregationData.R
@@ -1,6 +1,6 @@
 # build aggregation settings for the API call
 
-buildAggregationSettings <- function(valuesFrom, extractedValue, aggregationType, xDimension, yDimension, logId, zDimension, aggrLevel, followers, type, cache, maxValueAmount, activityExclusionFilter, traceFilterSequence, limit, page) {
+buildAggregationSettings <- function(valuesFrom, extractedValue, aggregationType, outerBinning, innerBinning, outerDateType, innerDateType, timeZone, maxAmountAttributes, xDimension, yDimension, logId, zDimension, aggrLevel, followers, type, cache, maxValueAmount, activityExclusionFilter, traceFilterSequence, limit, page) {
 
   if (zDimension != "null" ){
     zDimension = paste0('"', zDimension, '"')
@@ -10,24 +10,93 @@ buildAggregationSettings <- function(valuesFrom, extractedValue, aggregationType
     followers = paste0('"', zDimension, '"')
   }
 
+  # process the valueFrom attribute
+  # if input within "allCases", "allEvents", "allFollowers", put it directly as type
+  # if input contains a comma, separate into pre and succ
+  # if input does not contain commas, treat it as an activity
   if (valuesFrom %in% c("allCases", "allEvents", "allFollowers")){
-    valuesFrom = paste0('{"type": "', valuesFrom, '"}')
+    valuesFrom = paste0('
+      {
+      "type": "', valuesFrom, '"
+      }')
   } else if (grepl(",", valuesFrom)){
     events = unlist(strsplit(valuesFrom, "[,]"))
-    valuesFrom = paste0('{"type": "follower", 
+    valuesFrom = paste0('
+      {
+      "type": "follower", 
       "preActivity": "', events[1], '",
-      "succActivity": "', events[2], '"}')
+      "succActivity": "', events[2], '"
+      }')
   } else {
-    valuesFrom = paste0('{"type": "event",
-      "activity": "', valuesFrom, '"}')
+    valuesFrom = paste0('
+      {
+      "type": "event",
+      "activity": "', valuesFrom, '"
+      }')
   }
 
+  # process the extractedValue attribute
+  # if input within "frequency", "duration", "startDate", "endDate", put it directly as type
+  # else treat it as the attribute name
   if (extractedValue %in% c("frequency", "duration", "startDate", "endDate")){
-    extractedValue = paste0('{"type": "', extractedValue, '"}')
+    extractedValue = paste0('
+      {
+      "type": "', extractedValue, '"
+      }')
   } else {
-    extractedValue = paste0('{"type": "attributeValue", 
-      "attribute": "', extractedValue, '"}') 
+    extractedValue = paste0('
+      {
+      "type": "attributeValue", 
+      "attribute": "', extractedValue, '"
+      }') 
   }
+
+  # process the outerbinning and innerbinning according to API documentation
+  if (outerBinning != "null") {
+    if (outerBinning %in% c("byActivity", "byDuration")){
+      outerBinning = paste0('
+        {
+        "type": "', outerBinning, '"
+        }')
+    } else if (outerBinning %in% c("byYear", "byMonth", "byQuarter", "byDayOfWeek", "byDayOfYear", "byHourOfDay")){
+      outerBinning = paste0('
+      {
+      "type": "', outerBinning, '", 
+      "dateType": "', outerDateType, '",
+      "timeZone": "', timeZone, '"
+      }')
+    } else {
+      outerBinning = paste0('
+      {
+      "type": "byAttribute", 
+      "attribute": "', outerBinning, '"
+      }')
+    }
+  }
+
+  if (innerBinning != "null") {
+    if (innerBinning %in% c("byActivity", "byDuration")){
+      innerBinning = paste0('
+        {
+        "type": "', innerBinning, '"
+        }')
+    } else if (innerBinning %in% c("byYear", "byMonth", "byQuarter", "byDayOfWeek", "byDayOfYear", "byHourOfDay")){
+      innerBinning = paste0('
+      {
+      "type": "', innerBinning, '", 
+      "dateType": "', innerDateType, '",
+      "timeZone": "', timeZone, '"
+      }')
+    } else {
+      innerBinning = paste0('
+      {
+      "type": "byAttribute", 
+      "attribute": "', innerBinning, '"
+      }')
+    }
+  }
+
+  maxAmountAttributes = paste0('{"maxAmountAttributes": ', maxAmountAttributes, '}')
 
   miningBody <- paste0('
          {
@@ -52,21 +121,43 @@ buildAggregationSettings <- function(valuesFrom, extractedValue, aggregationType
           }
          }')
 
-  rqBody <- paste0('{
-    "valuesFrom": ', valuesFrom, ',
-    "extractedValue": ', extractedValue, ',
-    "aggregationType": ', aggregationType, ',
-    "miningRequest": ', miningBody,'
-    }')
+  if (outerBinning == "null"){
+    rqBody <- paste0('
+      {
+      "valuesFrom": ', valuesFrom, ',
+      "extractedValue": ', extractedValue, ',
+      "aggregationType": "', aggregationType, '",
+      "options": ', maxAmountAttributes, ',
+      "miningRequest": ', miningBody,'
+      }')  
+  } else {
+    rqBody <- paste0('
+      {
+      "valuesFrom": ', valuesFrom, ',
+      "extractedValue": ', extractedValue, ',
+      "aggregationType": "', aggregationType, '",
+      "outerBinning": ', outerBinning, ',
+      "innerBinning": ', innerBinning, ',
+      "options": ', maxAmountAttributes, ',
+      "miningRequest": ', miningBody,'
+      }')  
+  }
+
 }
 
 #' @title Aggregate
 #' Aggregate data once uploaded to Lana
 #' Aggregations can be calculated by time (month, day of week, hour) or by attribute regarding the frequency, average duration, median duration and total duration. Also the aggregated data can be grouped by attributes.
 #' @description Gets the aggregation of the requested data with the specified parameters . \cr See https://api.lana-labs.com/#/routes/getAggregatedData
-#' @param valuesFrom ("allCases" / "allEvents" / "allFollowers" / "event" / "followers")
-#' @param extractedValue ("frequency" / "duration" / "startDate" / "endDate" / "custom")
-#' @param aggregationType ("min" / "max" / "sum" / "mean" / "median")
+#' @param valuesFrom ("allCases" / "allEvents" / "allFollowers" / "event" / "followers") - if input is event, valuesFrom should be the activity name; if input is followers, valuesFrom should contain both pre and succ separated by comma.
+#' @param extractedValue ("frequency" / "duration" / "startDate" / "endDate" / "attributeValue") - if input is a attribute value, extractedValue should be that attribute name
+#' @param aggregationType (optional, "min" / "max" / "sum" / "mean" / "median")
+#' @param outerBinning (optional, "byActivity" / "byDuration" / "byYear" / "byMonth" / "byQuarter" / "byDayOfWeek" / "byDayOfYear" / "byHourOfDay")
+#' @param outerDateType (optional, the date type for outerbinning)
+#' @param innerBinning (optional, "byActivity" / "byDuration" / "byYear" / "byMonth" / "byQuarter" / "byDayOfWeek" / "byDayOfYear" / "byHourOfDay")
+#' @param innerDateType (optional, the date type for innerbinning)
+#' @param timeZone (optional, time zone id for outer and inner binning)
+#' @param maxAmountAttributes (optional, default = 4)
 #' @param lanaUrl URL of the instance that LANA is running on
 #' @param lanaToken Lana API token read from LANA
 #' @param logId Log ID being read from LANA
@@ -90,13 +181,13 @@ buildAggregationSettings <- function(valuesFrom, extractedValue, aggregationType
 #' aggregate("Incident_withImpactAttributes.csv", xDimension = "byTime=byMonth", yDimension = "totalDuration")
 #' aggregate("Incident_withImpactAttributes.csv", xDimension = "byTime=byMonth", yDimension = "frequency", zDimension = "byAttribute=Est. Cost")
 
-aggregate <- function(lanaUrl, lanaToken, logId, xDimension, yDimension, valuesFrom, extractedValue, aggregationType="null", zDimension="null", aggrLevel="traces", followers="null",
+aggregate <- function(lanaUrl, lanaToken, logId, xDimension, yDimension, valuesFrom, extractedValue, aggregationType="null", outerBinning="null", innerBinning="null", outerDateType="null", innerDateType="null", timeZone="null", maxAmountAttributes=4, zDimension="null", aggrLevel="traces", followers="null",
                       type="aggregation", cache="{}", maxValueAmount=5, activityExclusionFilter="[]", traceFilterSequence="[]",
                       limit = 10, page = 1){
 
   # Make request to get aggregated data from LANA
 
-  rqBody <- buildAggregationSettings(valuesFrom, extractedValue, aggregationType, xDimension, yDimension, logId, zDimension, aggrLevel, followers, type, cache,
+  rqBody <- buildAggregationSettings(valuesFrom, extractedValue, aggregationType, outerBinning, innerBinning, outerDateType, innerDateType, timeZone, xDimension, yDimension, logId, zDimension, aggrLevel, followers, type, cache,
                                      maxValueAmount, activityExclusionFilter, traceFilterSequence, limit, page)
 
   aggregationRequestData <- httr::GET(paste0(lanaUrl, "/api/v2/aggregatedData?request=", URLencode(rqBody, reserved = T)),
