f12876ab1030ab3b5dcca97df1b4ec49a9395b43
malte <malte@lana-labs.com>
2020-07-10 15:39:25 +0200

    Added packages for import to the description, added context to the test file

--- tests/testthat/test_aggregation.R
+++ tests/testthat/test_aggregation.R
@@ -1,3 +1,4 @@
+context("Test context for basic functions")
 source("config.R")
 
 #the expected data frames were generated with the old aggregate-function calling the /api/aggregatedData endpoint or via the LANA frontend
@@ -12,16 +13,16 @@ test_that("aggregations with attribute and time groupings return the expected da
                               logId = selectedLogId,
                               aggregationFunction = "mean",
                               aggrLevel = "events")
-  
-  caseCount <- c(891, 557, 363, 189) 
-  Country <- c("Germany", "Austria", "Netherlands", "Switzerland") 
-  frequency <- c(891, 557, 363, 189) 
+
+  caseCount <- c(891, 557, 363, 189)
+  Country <- c("Germany", "Austria", "Netherlands", "Switzerland")
+  frequency <- c(891, 557, 363, 189)
   byMonth <- c("Jan 2016", "Jan 2016", "Jan 2016", "Jan 2016")
-  
+
   df_expected <- data.frame(caseCount, Country, frequency, byMonth, stringsAsFactors = FALSE) %>%
     mutate(caseCount = as.integer(caseCount), frequency = as.integer(frequency)) %>%
     as_tibble()
-  
+
   testthat::expect_identical(df_expected, df_aggregation)
 
 })
@@ -34,16 +35,16 @@ test_that("aggregations with numeric attribute metrics return the expected dataf
                               logId = selectedLogId,
                               aggregationFunction = "sum",
                               aggrLevel = "traces")
-  
-  caseCount <- c(2000) 
-  noAggregation <- c("No grouping") 
+
+  caseCount <- c(2000)
+  noAggregation <- c("No grouping")
   Cost <- c(8536000)
-  
+
   df_expected <- data.frame(caseCount, noAggregation, Cost, stringsAsFactors = FALSE) %>%
     mutate(caseCount = as.integer(caseCount), Cost = as.integer(Cost))
-  
+
   testthat::expect_identical(df_expected, df_aggregation)
-  
+
 })
 
 test_that("aggregations with specified valueSorting and sortingOrder return the expected dataframe", {
@@ -56,16 +57,16 @@ test_that("aggregations with specified valueSorting and sortingOrder return the 
                               aggrLevel = "allCases",
                               valueSorting = "alphabetic",
                               sortingOrder = "ascending")
-  
-  caseCount <- c(109, 295, 57, 610, 929) 
-  Classification <- c("Backup", "Citrix", "Intranet", "Mail", "SAP") 
+
+  caseCount <- c(109, 295, 57, 610, 929)
+  Classification <- c("Backup", "Citrix", "Intranet", "Mail", "SAP")
   avgDuration <- c(23770458.715596333, 63067525.423728816, 22307368.421052627, 27052327.868852418, 49583186.221743822)
-  
+
   df_expected <- data.frame(caseCount, Classification, avgDuration, stringsAsFactors = FALSE) %>%
     mutate(caseCount = as.integer(caseCount))
-  
+
   testthat::expect_identical(df_expected, df_aggregation)
-  
+
 })
 
 test_that("aggregations with a traceFilterSequence and maxValueAmount return the expected dataframe", {
@@ -77,14 +78,14 @@ test_that("aggregations with a traceFilterSequence and maxValueAmount return the
                               lanaUrl = lanaUrl,
                               logId = selectedLogId,
                               aggrLevel = "events")
-  
-  caseCount <- c(1080, 978, 762, 368, 281, 142, 140, 139, 133, 1677) 
-  byHour <- c("13", "12", "14", "11", "15", "23", "16", "6", "4", "Other") 
+
+  caseCount <- c(1080, 978, 762, 368, 281, 142, 140, 139, 133, 1677)
+  byHour <- c("13", "12", "14", "11", "15", "23", "16", "6", "4", "Other")
   Cost <- c(146000, 120000, 162000, 176000, 109000, 94000, 64000, 168000, 149000, 2223000)
-  
+
   df_expected <- data.frame(caseCount, byHour, Cost, stringsAsFactors = FALSE) %>%
     mutate(caseCount = as.integer(caseCount), Cost = as.integer(Cost))
-  
+
   testthat::expect_identical(df_expected, df_aggregation)
-  
+
 })
