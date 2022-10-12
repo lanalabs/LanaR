228b418ef57a34d22256b15c1932f61642f4c396
emcmd <michael.ehrenstein@lanalabs.com>
2022-10-12 14:13:10 -0400

    Updated getLogs, renamed and updated getLogIdByName. Added function to get user ID automatically. Added documentation.

--- R/logfiles.R
+++ R/logfiles.R
@@ -1,43 +1,50 @@
 #' Get all logs
-#' @return data frame with all log files owned by or shared with the authenticated user
-#' @name getLogs
 #'
-#' DEPRECEATED
+#' @param lanaUrl URL of the instance that LANA is running on
+#' @param lanaToken Lana API token read from LANA
+#' 
+#' @return Data Frame with all logs owned by or shared with the authenticated user
 
-getLogs <- function() {
-
-  checkAuthentication()
-  lanaApiUrl <- Sys.getenv("LANA_URL")
-  lanaAuthorization <- Sys.getenv("LANA_TOKEN")
-  lanaUserId <- Sys.getenv("LANA_USERID")
-
-  logData <- httr::GET(paste0(lanaApiUrl, "/api/users/", lanaUserId, "/logs"), httr::add_headers(Authorization = lanaAuthorization))
-
-  checkHttpErrors(logData)
-
-  jsonlite::fromJSON(
-    httr::content(logData, encoding = "UTF-8", type = "text")
-  )
-
+getLogs <- function(lanaUrl, lanaToken) {
+  
+  response <- httr::GET(
+    paste0("https://", lanaUrl, "/api/logs"),
+    httr::add_headers(Authorization = lanaToken))
+  lanar::checkHttpErrors(response)
+  content <- jsonlite::fromJSON(httr::content(response, encoding = "UTF-8", type = "text"))
+  return(content)
 }
 
-# getLogs <- function(lanaUrl, lanaToken, lanaUserId) {
-#
-#   lanar::checkAuthentication(lanaUrl, lanaToken)
-#
-#   logData <- httr::GET(paste0(lanaUrl, "/api/users/", lanaUserId, "/logs"), httr::add_headers(Authorization = lanaToken))
-#
-#   lanar::checkHttpErrors(logData)
-#
-#   jsonlite::fromJSON(httr::content(logData, encoding = "UTF-8", type = "text"))
-#
-# }
+
+#' Get ID of authenticated user
+#' 
+#' @param lanaUrl URL of the instance that LANA is running on
+#' @param lanaToken Lana API token read from LANA
+#' 
+#' @return A string with the user ID
+
+getUserId <- function(lanaUrl, lanaToken) {
+  
+  response <- httr::GET(
+    paste0("https://", lanaUrl, "/api/users/by-token"),
+    httr::add_headers(Authorization = lanaToken)  )
+  lanar::checkHttpErrors(response)
+  content <- jsonlite::fromJSON(httr::content(response, as = "text", encoding = "UTF-8"))
+  return(content$id)
+}
 
 
-#' Choose specific log file
-chooseLog <- function(logName){
+#' Get the ID of a log by name
+#' 
+#' @param logName Name of the log
+#' @param lanaUrl URL of the instance that LANA is running on
+#' @param lanaToken Lana API token read from LANA
+#' 
+#' @return A string with the log ID, or IDs in case of multiple identical names
 
-  userLogs <- lanar::getLogs()
-  logId<- max(userLogs[grepl(logName, userLogs$name, fixed=T), ]$id)
-
+getLogIdByName <- function(lanaUrl, lanaToken, logName){
+  
+  userLogs <- lanar::getLogs(lanaUrl, lanaToken)
+  logId <- max(userLogs[grepl(logName, userLogs$name, fixed=T), ]$id)
+  return(logId)
 }
