7b816c6e175dd14674df64bd8fa51f79dbfe1e75
emcmd <michael.ehrenstein@lanalabs.com>
2022-10-12 14:10:52 -0400

    Updated checkHttpErrors to only check status code of API call.

--- R/errorChecking.R
+++ R/errorChecking.R
@@ -1,19 +1,10 @@
+#' Check response of API call, produce warning on error
+#' 
+#' @param response httr response object from of API call
+
 checkHttpErrors <- function(response) {
 
   if(response["status_code"] != 200) {
       stop(paste0(httr::content(response), " \nHTTP error details = ", httr::http_status(response)$message))
-  }
-
-  if(isEmptyLog(response)) {
-      warning("No response data. Your filter settings led to an empty log.\n")
-  }
-}
-
-isEmptyLog <- function(response) {
-  responseData <- jsonlite::fromJSON(httr::content(response, as = "text", encoding = "UTF-8"))
-  if(length(responseData$chartValues)==0) {
-    return(TRUE)
-  } else {
-    return(FALSE)
   }
 }
