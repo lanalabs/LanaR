daf5ac3bd14dcd7aaa361419eb30c2eb800915b7
emcmd <michael.ehrenstein@lanalabs.com>
2022-10-18 14:03:27 +0100

    Updated variable names for API host and token.

--- R/logfiles.R
+++ R/logfiles.R
@@ -1,15 +1,15 @@
 #' Get all logs
 #'
-#' @param lanaUrl URL of the instance that LANA is running on
-#' @param lanaToken Lana API token read from LANA
+#' @param host URL of the instance that Appian Process Mining is running on
+#' @param token Appian Process Mining API token
 #' 
 #' @return Data Frame with all logs owned by or shared with the authenticated user
 
-getLogs <- function(lanaUrl, lanaToken) {
+getLogs <- function(host, token) {
   
   response <- httr::GET(
-    paste0("https://", lanaUrl, "/api/logs"),
-    httr::add_headers(Authorization = lanaToken))
+    paste0("https://", host, "/api/logs"),
+    httr::add_headers(Authorization = token))
   lanar::checkHttpErrors(response)
   content <- jsonlite::fromJSON(httr::content(response, encoding = "UTF-8", type = "text"))
   return(content)
@@ -18,16 +18,16 @@ getLogs <- function(lanaUrl, lanaToken) {
 
 #' Get ID of authenticated user
 #' 
-#' @param lanaUrl URL of the instance that LANA is running on
-#' @param lanaToken Lana API token read from LANA
+#' @param host URL of the instance Appian Process Mining is running on
+#' @param token Appian Process Mining API token
 #' 
 #' @return A string with the user ID
 
-getUserId <- function(lanaUrl, lanaToken) {
+getUserId <- function(host, token) {
   
   response <- httr::GET(
-    paste0("https://", lanaUrl, "/api/users/by-token"),
-    httr::add_headers(Authorization = lanaToken)  )
+    paste0("https://", host, "/api/users/by-token"),
+    httr::add_headers(Authorization = token)  )
   lanar::checkHttpErrors(response)
   content <- jsonlite::fromJSON(httr::content(response, as = "text", encoding = "UTF-8"))
   return(content$id)
@@ -37,14 +37,14 @@ getUserId <- function(lanaUrl, lanaToken) {
 #' Get the ID of a log by name
 #' 
 #' @param logName Name of the log
-#' @param lanaUrl URL of the instance that LANA is running on
-#' @param lanaToken Lana API token read from LANA
+#' @param host URL of the instance that Appian Process Mining is running on
+#' @param token Appian Process Mining API token
 #' 
 #' @return A string with the log ID, or IDs in case of multiple identical names
 
-getLogIdByName <- function(lanaUrl, lanaToken, logName){
+getLogIdByName <- function(host, token, logName){
   
-  userLogs <- lanar::getLogs(lanaUrl, lanaToken)
+  userLogs <- lanar::getLogs(host, token)
   logId <- max(userLogs[grepl(logName, userLogs$name, fixed=T), ]$id)
   return(logId)
 }
